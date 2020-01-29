1a007e23c41b81bea4b5f8d37bd96d9fcdf2c8f6
Rob Hess <robwhess@gmail.com>
2020-01-29 15:10:31 -0800

    Add code from 1/29 lecture.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
 import React from 'react';
+import {
+  Switch,
+  Route,
+  Link,
+  NavLink,
+  Redirect,
+  useParams,
+  useRouteMatch
+} from 'react-router-dom';
 
 import './App.css';
 
@@ -21,12 +30,89 @@ function People() {
 }
 
 function Menu() {
-  return <h1>Menu</h1>;
+  const { url, path } = useRouteMatch();
+  return (
+    <div>
+      <h1>Menu</h1>
+      <ul>
+        <li>
+          <Link to={`${url}/pizza`}>Pizza</Link>
+        </li>
+        <li>
+          <Link to={`${url}/tacos`}>Tacos</Link>
+        </li>
+        <li>
+          <Link to={`${url}/sushi`}>Sushi</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route exact path={`${path}`}>
+          <h2>Select a menu item above</h2>
+        </Route>
+        <Route path={`${path}/:menuItem`}>
+          <MenuItem />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+function MenuItem() {
+  const { menuItem } = useParams();
+  const { url, path } = useRouteMatch();
+  return (
+    <div>
+      <h2>A menu item was selected: {menuItem}</h2>
+      <img src={menu[menuItem]} alt={menuItem} />
+      <p>url: {url}</p>
+      <p>path: {path}</p>
+    </div>
+  );
 }
 
 function App() {
   return (
-    <Home />
+    <div>
+      <h1>Site title</h1>
+      <ul>
+        <li>
+          <NavLink exact to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/people">People</NavLink>
+        </li>
+        <li>
+          <NavLink to="/about">About</NavLink>
+        </li>
+        <li>
+          <NavLink to="/index">Index</NavLink>
+        </li>
+        <li>
+          <NavLink to="/menu">Menu</NavLink>
+        </li>
+      </ul>
+      <hr />
+      <Switch>
+        <Route path="/people">
+          <People />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/index">
+          <Redirect to="/" />
+        </Route>
+        <Route path="/menu">
+          <Menu />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="*">
+          <div>404</div>
+        </Route>
+      </Switch>
+    </div>
   );
 }
 
